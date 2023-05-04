8a0f0607e930cf20ed249d868ce0bd9b154596b4
llyttle <llyttle@olin.edu>
2023-05-04 15:30:18 -0400

    MUCH IMPROVED warehouse info generator using recursion

--- main.py
+++ main.py
@@ -10,27 +10,28 @@ from pytictoc import TicToc
 import numpy as np
 import matplotlib.pyplot as plt
 
-t = TicToc()
-
 evaluator = Evaluator()
 
-# sp = evaluator.simulation_parameters
-
-# Set all variables to a specified default value
-evaluator.default_parameters()
-
-evaluator.set_variable("warehouse_x",  min=2, max=10, step=2)
+# evaluator.set_static("warehouse_x",  4)
+# evaluator.set_static("warehouse_y",  8)
+# evaluator.set_variable("warehouse_x",  min=2, max=10, step=2)
 # evaluator.set_variable("warehouse_y",  min=2, max=10, step=2)
 
-evaluator.set_static("n_drone", 20)
-evaluator.set_static("n_amr", 5)
+# evaluator.set_variable("warehouse_x",  min=2, max=10, step=2)
+# evaluator.set_variable("warehouse_y",  min=2, max=10, step=2)
+# evaluator.set_variable("warehouse_z",  min=50, max=100, step=10)
 
-# evaluator.set_variable("n_drone",  min=10, max=100, step=10)
-# evaluator.set_variable("n_amr", min=1,  max=15,  step=5)
+wh_info_list = evaluator.generate_wh_info()
+
+# evaluator.set_static("n_drone", 20)
+# evaluator.set_static("n_amr", 5)
+evaluator.set_variable("n_drone",  min=10, max=100, step=10)
+evaluator.set_variable("n_amr", min=1,  max=15,  step=5)
+
+fleet_comp_list = evaluator.generate_fleet_composition()
 
 visualizer_info = []
-
-for wh_info in evaluator.generate_wh_info():
+for wh_info in wh_info_list:
     # Initialize a warehouse and pick and drop points
     wh_map = WarehouseMap(wh_info, resolution=0.1, units="ft")
     wh_pick_points, wh1_drop_points = wh_map.generate_points()
@@ -40,9 +41,9 @@ for wh_info in evaluator.generate_wh_info():
     rand_task_list = TaskList()
     rand_task_list.populate_randomly(wh_pick_points, wh1_drop_points, 100)
     
-    print(f"Warehouse Dimensions: {wh_info['warehouse_x']}x {wh_info['warehouse_y']}y")
+    print(f"Warehouse Dimensions: {wh_info['warehouse_x']}x {wh_info['warehouse_y']}y {wh_info['warehouse_z']}z")
 
-    for fleet_comp in evaluator.generate_fleet_composition():
+    for fleet_comp in fleet_comp_list:
         print(f"    Fleet Composition: Drones = {fleet_comp[0][1]}, AMRs = {fleet_comp[1][1]}")
         
         fleet = Fleet()
@@ -69,10 +70,6 @@ wh_map, rand_task_list, fleet = visualizer_info[0]
 # task_visualizer = Visualizer(wh_map, rand_task_list, fleet, vis_type="fleet_tasks")
 # task_visualizer.show()
 
-# ax.set_zlim(0, 0.04)
-
-# plt.show()
-
 # print(fleet.get_robots_as_list("Drone"))
 
 # path_planner = PathPlanner(wh1_map, fleet)
@@ -82,8 +79,8 @@ wh_map, rand_task_list, fleet = visualizer_info[0]
 # task_visualizer.show()
 
 
-path_visualizer = Visualizer(wh_map, rand_task_list, fleet, vis_type="black_tasks_traces_on", show_t=True)
-path_visualizer.show()
+# path_visualizer = Visualizer(wh_map, rand_task_list, fleet, vis_type="black_tasks_traces_on", show_t=True)
+# path_visualizer.show()
 
-animation_visualizer = Visualizer(wh_map, rand_task_list, fleet, vis_type="color_tasks_traces_off", show_t=True)
-animation_visualizer.animation()
+# animation_visualizer = Visualizer(wh_map, rand_task_list, fleet, vis_type="color_tasks_traces_off", show_t=True)
+# animation_visualizer.animation()
